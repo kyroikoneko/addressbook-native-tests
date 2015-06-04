d7edf7a36fce3d80e1448cb9f21a4df08c65db7f
kyroikoneko <kotenok.nataly@gmail.com>
2015-06-05 01:07:30 +0400

    7.1

---
+++ src/com/example/fw/HelpersBase.java
@@ -0,0 +1,11 @@
+package com.example.fw;
+
+
+public class HelpersBase {
+
+	protected final ApplicationManager manager;
+
+	public HelpersBase(ApplicationManager applicationManager) {
+		this.manager = applicationManager;
+	}
+}
